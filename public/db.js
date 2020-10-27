653dfbaddac0f3b270f0faedb6e76d639cba6d0c
Eddie Sevilla <eddiesevilla@Eddies-MacBook-Air.local>
2020-10-27 18:57:21 -0500

    changes for herku

--- public/db.js
+++ public/db.js
@@ -14,7 +14,7 @@ request.onsuccess = function (event) {
 };
 
 request.onerror = function (event) {
-    console.log("Whoops!" + event.target.errorCode);
+    console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
